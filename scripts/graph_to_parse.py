b1428cecec4ea736464c0c22f4b77ca5ec7df303
Jesse Dunietz <yavyash@gmail.com>
2015-02-23 00:14:00 -0500

    Added graph/sentence to parse script

---
+++ scripts/graph_to_parse.py
@@ -0,0 +1,13 @@
+from StringIO import StringIO
+from parse_to_dot import make_dot
+
+def parsify(sentence):
+    lines = []
+    for (start, end), label in sentence.edge_labels.items():
+        lines.append('%s(%s_%s, %s_%s)' % (
+            label, sentence.tokens[start].lemma, start,
+            sentence.tokens[end].lemma, end))
+    return lines
+
+def visualize_sentence(sentence):
+    make_dot(StringIO(parsify(sentence)), 'in-memory')
