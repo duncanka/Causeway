9963b5617a24368872d1b2fc6a913fae0220dab9
Jesse Dunietz <yavyash@gmail.com>
2015-03-02 15:24:20 -0500

    Formatted TSurgeon scripts more nicely

--- src/pairwise/tsurgeon/normalize_passives.ts
+++ src/pairwise/tsurgeon/normalize_passives.ts
@@ -1,4 +1,6 @@
-__=subjpass < nsubjpass=subjpassdep > (__ <2 /VB*/ < (/be_[0-9]+/=be <1 auxpass) < (/by_[0-9]+/=by < (__=agent <1 pobj=agentdep)))
+__=subjpass < nsubjpass=subjpassdep
+            > (__ <2 /VB*/ < (/be_[0-9]+/=be <1 auxpass)
+                           < (/by_[0-9]+/=by < (__=agent <1 pobj=agentdep)))
 
 relabel agentdep nsubj
 relabel subjpassdep dobj
--- src/pairwise/tsurgeon/normalize_vmod_passives.ts
+++ src/pairwise/tsurgeon/normalize_vmod_passives.ts
@@ -1,4 +1,6 @@
-__=subjpass < (__=passiveverb <1 vmod <2 VBN < (/by_[0-9]+/=by < (__=agent <1 pobj=agentdep)))
+__=subjpass < (__=passiveverb <1 vmod
+                              <2 VBN
+                              < (/by_[0-9]+/=by < (__=agent <1 pobj=agentdep)))
 
 relabel agentdep nsubj
 move agent >3 passiveverb
