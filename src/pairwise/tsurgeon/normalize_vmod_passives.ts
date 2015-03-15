a08beb18421887f2fa6a9af62ba7c8ca7826b009
Jesse Dunietz <yavyash@gmail.com>
2015-03-14 21:24:42 -0400

    Restricted POS types for vmod passives

--- src/pairwise/tsurgeon/normalize_vmod_passives.ts
+++ src/pairwise/tsurgeon/normalize_vmod_passives.ts
@@ -1,4 +1,6 @@
-__=subjpass < (__=passiveverb <1 vmod
+% TODO: Should more POS types be allowed here?
+__=subjpass [<2 /NN.*/ | <2 CD | <2 PRP | <2 VBG]
+            < (__=passiveverb <1 vmod
                               <2 VBN
                               < (/by_[0-9]+/=by < (__=agent <1 pobj=agentdep)))
 
