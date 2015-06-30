d8f8cdb63a1f084d2af553cf52dcd715a9391993
Jesse Dunietz <yavyash@gmail.com>
2015-06-30 17:40:56 -0400

    Fixed combiner bug where it would error out on possible causations missing one argument

--- src/causality_pipelines/baseline/combiner.py
+++ src/causality_pipelines/baseline/combiner.py
@@ -18,34 +18,38 @@ class BaselineCombinerModel(Model):
 
     def train(self, parts):
         pass
-    
+
     @staticmethod
     def _get_instance_tuple(causation_instance, sentence):
-        arg_indices = (sentence.get_head(causation_instance.cause).index,
-                      sentence.get_head(causation_instance.effect).index)
+        arg_indices = tuple(
+            sentence.get_head(arg) if arg else None
+            for arg in [causation_instance.cause, causation_instance.effect])
         connective_indices = tuple(t.index for t in
                                    causation_instance.connective)
         return arg_indices + connective_indices
-    
+
     def test(self, parts):
+        # TODO: make this avoid presenting two annotations that differ only in
+        # that one of them is missing an argument. Particularly, make it prefer
+        # the one that has an argument.
         last_sentence = None
-        existing_causations = None
+        existing_causations_in_sentence = None
         for possible_causation in parts:
             sentence = possible_causation.sentence
             if sentence is not last_sentence:
-                existing_causations = []
+                existing_causations_in_sentence = []
                 for causation in sentence.causation_instances:
-                    existing_causations.append(
+                    existing_causations_in_sentence.append(
                         self._get_instance_tuple(causation, sentence))
                 
             if (self._get_instance_tuple(possible_causation, sentence)
-                not in existing_causations):
+                not in existing_causations_in_sentence):
                 possible_causation.sentence.add_causation_instance(
                     connective=possible_causation.connective,
                     cause=possible_causation.cause,
                     effect=possible_causation.effect)
 
-    
+
 class BaselineCombinerStage(Stage):
     def __init__(self, name, baseline_causations_attr_name):
         super(BaselineCombinerStage, self).__init__(name,
