2adfcaef5f44d26ac876ca75c7a196f963d7df3f
Jesse Dunietz <yavyash@gmail.com>
2015-07-30 03:19:37 -0400

    Updated depassivizing TSurgeon script to handle cases where something's hanging off the "by"

--- src/causality_pipelines/tregex_based/tsurgeon_dep/normalize_passives.ts
+++ src/causality_pipelines/tregex_based/tsurgeon_dep/normalize_passives.ts
@@ -1,9 +1,15 @@
 __=subjpass < nsubjpass=subjpassdep
-            > (__ <2 /VB*/ < (/be_[0-9]+/=be <1 auxpass)
-                           < (/by_[0-9]+/=by < (__=agent <1 pobj=agentdep)))
+            > (__ <2 /VB*/ < (/be_[0-9]+/=be <1 auxpass=berel <2 __=bepos)
+                           < (/by_[0-9]+/=by <1 __=byrel <2 __=bypos < (__=agent <1 pobj=agentdep)))
 
 relabel agentdep nsubj
 relabel subjpassdep dobj
 move agent $+ subjpass
-delete by
-delete be
+% Remove POS/edge label children of by and be before merging up any children
+delete berel
+delete bepos
+delete byrel
+delete bypos
+% Delete by and be and integrate their children. Nowhere better to put them than into the parent, if they exist. (Probably parse errors anyway.)
+excise by by
+excise be be
