cb2b68561fa518a772f60594d301e95179a33adc
Jesse Dunietz <yavyash@gmail.com>
2015-08-26 00:17:32 -0400

    Removed partial match printing by default

--- src/causality_pipelines/__init__.py
+++ src/causality_pipelines/__init__.py
@@ -7,6 +7,13 @@ from iaa import CausalityMetrics
 from pipeline import Stage, Evaluator
 from util import listify, print_indented
 
+try:
+    DEFINE_bool("iaa_calculate_partial", False,
+                "Whether to compute metrics for partial overlap")
+except DuplicateFlagError as e:
+    logging.warn('Ignoring redefinition of flag %s' % e.flagname)
+
+
 class PossibleCausation(object):
     '''
     Designed to mimic actual CausationInstance objects.
@@ -32,9 +39,12 @@ class IAAEvaluator(Evaluator):
     def __init__(self, compare_degrees, compare_types, log_test_instances,
                  compare_args, pairwise_only,
                  causations_property_name='causation_instances'):
-        self._with_partial_metrics = CausalityMetrics(
-            [], [], True, log_test_instances, None, compare_degrees,
-            compare_types, True, True, log_test_instances)
+        if FLAGS.iaa_calculate_partial:
+            self._with_partial_metrics = CausalityMetrics(
+                [], [], True, log_test_instances, None, compare_degrees,
+                compare_types, True, True, log_test_instances)
+        else:
+            self._with_partial_metrics = None
         self._without_partial_metrics = CausalityMetrics(
             [], [], False, log_test_instances, None, compare_degrees,
             compare_types, True, True, log_test_instances)
@@ -46,13 +56,14 @@ class IAAEvaluator(Evaluator):
         self.pairwise_only = pairwise_only
 
     def evaluate(self, sentences, original_sentences):
-        with_partial = CausalityMetrics(
-            original_sentences, sentences, True, self.log_test_instances,
-            compare_types=self.compare_types, compare_args=self.compare_args,
-            compare_degrees=self.compare_degrees,
-            pairwise_only=self.pairwise_only,
-            save_agreements=self.log_test_instances,
-            causations_property_name=self.causations_property_name)
+        if FLAGS.iaa_calculate_partial:
+            with_partial = CausalityMetrics(
+                original_sentences, sentences, True, self.log_test_instances,
+                compare_types=self.compare_types, compare_args=self.compare_args,
+                compare_degrees=self.compare_degrees,
+                pairwise_only=self.pairwise_only,
+                save_agreements=self.log_test_instances,
+                causations_property_name=self.causations_property_name)
         without_partial = CausalityMetrics(
             original_sentences, sentences, False, self.log_test_instances,
             compare_types=self.compare_types, compare_args=self.compare_args,
@@ -67,30 +78,38 @@ class IAAEvaluator(Evaluator):
                                log_differences=True, log_agreements=True,
                                indent=1)
             print
-            print 'Differences allowing partial matches:'
-            with_partial.pp(log_stats=False, log_confusion=False,
-                            log_differences=True, log_agreements=True,
-                            indent=1)
+            if FLAGS.iaa_calculate_partial:
+                print 'Differences allowing partial matches:'
+                with_partial.pp(log_stats=False, log_confusion=False,
+                                log_differences=True, log_agreements=True,
+                                indent=1)
 
-        self._with_partial_metrics += with_partial
+        if FLAGS.iaa_calculate_partial:
+            self._with_partial_metrics += with_partial
         self._without_partial_metrics += without_partial
 
     _PERMISSIVE_KEY = 'Allowing partial matches'
     _STRICT_KEY = 'Not allowing partial matches'
 
     def complete_evaluation(self):
-        result = {self._PERMISSIVE_KEY: self._with_partial_metrics,
-                  self._STRICT_KEY: self._without_partial_metrics}
+        if FLAGS.iaa_calculate_partial:
+            result = {self._PERMISSIVE_KEY: self._with_partial_metrics,
+                      self._STRICT_KEY: self._without_partial_metrics}
+        else:
+            result = self._without_partial_metrics
         self._with_partial_metrics = None
         self._without_partial_metrics = None
         return result
 
     def aggregate_results(self, results_list):
-        permissive = CausalityMetrics.aggregate(
-            [result_dict[IAAEvaluator._PERMISSIVE_KEY]
-             for result_dict in results_list])
-        strict = CausalityMetrics.aggregate(
-            [result_dict[IAAEvaluator._STRICT_KEY]
-             for result_dict in results_list])
-        return {IAAEvaluator._PERMISSIVE_KEY: permissive,
-                IAAEvaluator._STRICT_KEY: strict}
+        if FLAGS.iaa_calculate_partial:
+            permissive = CausalityMetrics.aggregate(
+                [result_dict[IAAEvaluator._PERMISSIVE_KEY]
+                 for result_dict in results_list])
+            strict = CausalityMetrics.aggregate(
+                [result_dict[IAAEvaluator._STRICT_KEY]
+                 for result_dict in results_list])
+            return {IAAEvaluator._PERMISSIVE_KEY: permissive,
+                    IAAEvaluator._STRICT_KEY: strict}
+        else:
+            return CausalityMetrics.aggregate(results_list)
