5e73ef7f07fc453b55652c96b2222371dcf52f1b
Jesse Dunietz <yavyash@gmail.com>
2015-09-07 12:46:22 -0400

    Fixed up decision tree visualization script

--- scripts/show_dtree.py
+++ scripts/show_dtree.py
@@ -2,8 +2,10 @@ import os
 from sklearn import tree
 import subprocess
 import sys
+from pipeline.models import ClassBalancingModelWrapper
 
-def vis_tree(model, tree_classifier=None, filename='/tmp/tree.dot'):
+def vis_tree(model, tree_classifier=None, filename='/tmp/tree.dot',
+             open_imgs=True):
     tree.export_graphviz(
         tree_classifier, out_file=filename, max_depth=20,
         feature_names=model.feature_name_dictionary.ids_to_names)
@@ -12,17 +14,25 @@ def vis_tree(model, tree_classifier=None, filename='/tmp/tree.dot'):
     img_fname = "%s.png" % fname_base
     dot_proc = subprocess.Popen(["dot", "-Tpng", "-o%s" % img_fname, filename])
     (_std, err) = dot_proc.communicate()
+
     if err:
         sys.stderr.write(err)
     else:
-        with open(os.devnull, 'w') as null:
-            subprocess.call(['xdg-open', img_fname], stderr=null)
+        if open_imgs:
+            with open(os.devnull, 'w') as null:
+                subprocess.call(['xdg-open', img_fname], stderr=null)
 
-def vis_pipeline_trees(pipeline, stage_num, model_num=0):
+
+def vis_pipeline_trees(pipeline, stage_num, model_num=0, open_imgs=True):
     model = pipeline.stages[stage_num].models[model_num]
-    classifier = model.classifier.classifier
+    if isinstance(model.classifier, ClassBalancingModelWrapper):
+        classifier = model.classifier.classifier
+    else:
+        classifier = model.classifier
+
     if isinstance(classifier, tree.DecisionTreeClassifier):
         vis_tree(model, classifier)
     else:
-        for i, estimator in classifier.estimators_:
-            vis_tree(model, estimator, '/tmp/tree_%d.dot' % i)
+        for i, estimator in enumerate(classifier.estimators_):
+            vis_tree(model, estimator, '/tmp/tree_%d.dot' % i,
+                     open_imgs)
