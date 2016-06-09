08252c856bc218fc7b9f5cce2e1d61e147e50cf7
Jesse Dunietz <yavyash@gmail.com>
2016-06-09 16:31:23 -0400

    Added beginnings of better integration with scikit-learn pipelines
    (Currently for use in small parts of codebase, but hopefully the whole
    framework will ultimately move in this direction)

---
+++ src/skpipeline/__init__.py
@@ -0,0 +1,49 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.dummy import DummyClassifier
+from sklearn.pipeline import Pipeline, _name_estimators
+
+from pipeline.featurization import Featurizer, FeatureExtractor
+
+
+class FeaturizingTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, feature_extractors, selected_features_or_name_dict,
+                 save_featurized=False, default_to_matrix=True):
+        self.featurizer = Featurizer(
+            feature_extractors, selected_features_or_name_dict, save_featurized,
+            default_to_matrix)
+        self.feature_extractors = feature_extractors
+        self.selected_features_or_name_dict = selected_features_or_name_dict
+        self.save_featurized = save_featurized
+        self.default_to_matrix = default_to_matrix
+
+    def fit(self, instances, labels=None):
+        self.featurizer.register_features_from_instances(instances)
+        return self
+
+    def transform(self, instances):
+        return self.featurizer.featurize(instances)
+
+    def num_features(self):
+        return len(self.featurizer.feature_name_dictionary)
+
+
+dummy_feature_extractor = FeatureExtractor(
+    'dummy', lambda x: 0, FeatureExtractor.FeatureTypes.Numerical)
+def make_mostfreq_featurizing_estimator(estimator_name=None):
+    return make_featurizing_estimator(DummyClassifier('prior'),
+                                      [dummy_feature_extractor], ['dummy'],
+                                      estimator_name=estimator_name)
+
+
+def make_featurizing_estimator(
+    estimator, feature_extractors, selected_features_or_name_dict,
+    estimator_name=None, save_featurized=False, default_to_matrix=True):
+
+    featurizing_stage = FeaturizingTransformer(
+        feature_extractors, selected_features_or_name_dict, save_featurized,
+        default_to_matrix)
+    if estimator_name is not None:
+        named = [(estimator_name, estimator)]
+    else:
+        named = _name_estimators([estimator])
+    return Pipeline([(named[0][0] + '_featurizer', featurizing_stage)] + named)
