0fe6a51c820663f23e27dd9708e6f1f8859231db
Jesse Dunietz <yavyash@gmail.com>
2016-07-19 15:16:14 -0400

    Fixed/completed weight analysis script

--- scripts/analyze_weights.py
+++ scripts/analyze_weights.py
@@ -1,14 +1,21 @@
 from __future__ import print_function
+
 from collections import OrderedDict
+from sklearn.pipeline import Pipeline
+import sys
+
+from util import print_indented
+
 
 def get_weights_for_classifier(classifier_pipeline):
-    featurizer = classifier_pipeline.steps[0][1].featurizer
     classifier = classifier_pipeline.steps[1][1]
+    featurizer = classifier_pipeline.steps[0][1].featurizer
     support = classifier.named_steps['feature_selection'].get_support()
     lr = classifier.named_steps['classification'].classifier
     weights = [(featurizer.feature_name_dictionary.ids_to_names[ftnum],
                 lr.coef_[0][i])
-               for i, ftnum in enumerate(support.nonzero()[0])]
+               for i, ftnum in enumerate(support.nonzero()[0])
+               if lr.coef_[0][i] != 0.0]
     weights.sort(key=lambda tup: abs(tup[1])) # sort by weights' absolute values
     return OrderedDict(weights)
 
@@ -18,11 +25,28 @@ def get_general_classifier_weights(pipeline):
 
 def get_per_connective_weights(pipeline):
     stage = pipeline.stages[3]
-    return {connective: get_weights_for_classifier(classifier)
-            for connective, classifier in stage.model.classifiers.iteritems()}
+    weight_dicts = {}
+    for connective, classifier in stage.model.classifiers.iteritems():
+        if isinstance(classifier, Pipeline):
+            weight_dicts[connective] = None
+            continue        
+        
+        weight_dicts[connective] = get_weights_for_classifier(
+            dict(classifier.estimators)['per_conn'])
 
-def print_ordered_dict(d):
-    print('OrderedDict({')
+    return weight_dicts
+
+def print_ordered_dict(d, indent=0, file=sys.stdout):
+    print_indented(indent, 'OrderedDict({', file=file)
     for key, value in d.items():
-        print('  ', key, ': ', value, sep='')
-    print('})')
+        print_indented(indent + 1, key, ': ', value, ',', sep='', file=file)
+    print_indented(indent, '})', file=file)
+
+def print_per_connective_weights(pipeline, file=sys.stdout):
+    weight_dicts = get_per_connective_weights(pipeline)
+    for connective, weights in weight_dicts.iteritems():
+        if weights is None:
+            print(connective, ': None,', sep='', file=file)
+        else:
+            print(connective, ':', sep='', file=file, end='')
+            print_ordered_dict(weights, 1, file=file)
