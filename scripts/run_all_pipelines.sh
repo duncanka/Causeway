cfb0d3d4db92595bc9dd7dc1ebff3663511cfae8
Jesse Dunietz <yavyash@gmail.com>
2016-08-28 21:57:14 -0400

    Major improvements to run-all script (now using task-spooler)

--- scripts/run_all_pipelines.sh
+++ scripts/run_all_pipelines.sh
@@ -2,24 +2,33 @@
 
 SEED=2961393773
 OUT_DIR=../outputs/final
-DATA_DIR=/var/www/brat/data/finished
+DATA_DIR=/var/www/brat/data/Jesse/Test/finished
 PTB_DATA_DIR=/var/www/brat/data/Jeremy/PTB
 SHARED_FLAGS="--eval_with_cv --seed=$SEED --cv_folds=20"
 
-python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=baseline                                                 > $OUT_DIR/baseline_20f.txt 2> $OUT_DIR/baseline_20f.log &
+export TS_SLOTS=4
 
-for PIPELINE_TYPE in regex tregex; do
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE                                       > $OUT_DIR/${PIPELINE_TYPE}_20f.txt 2>$OUT_DIR/${PIPELINE_TYPE}_20f.log &
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE --filter_classifiers=mostfreq         > $OUT_DIR/${PIPELINE_TYPE}_mostfreq_20f.txt 2> $OUT_DIR/${PIPELINE_TYPE}_mostfreq_20f.log &
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE --filter_classifiers=global           > $OUT_DIR/${PIPELINE_TYPE}_global_20f.txt 2> $OUT_DIR/${PIPELINE_TYPE}_global_20f.log &
-    wait
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE --filter_classifiers=perconn          > $OUT_DIR/${PIPELINE_TYPE}_perconn_20f.txt 2> $OUT_DIR/${PIPELINE_TYPE}_perconn_20f.log &
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE --filter_classifiers=global,mostfreq  > $OUT_DIR/${PIPELINE_TYPE}_no_perconn_20f.txt 2> $OUT_DIR/${PIPELINE_TYPE}_no_perconn_20f.log &
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE --filter_classifiers=global,perconn   > $OUT_DIR/${PIPELINE_TYPE}_no_mostfreq_20f.txt 2> $OUT_DIR/${PIPELINE_TYPE}_no_mostfreq_20f.log &
-    wait
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE --filter_classifiers=mostfreq,perconn > $OUT_DIR/${PIPELINE_TYPE}_no_global_20f.txt 2> $OUT_DIR/${PIPELINE_TYPE}_no_global_20f.log &
-    python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=${PIPELINE_TYPE}+baseline                            > $OUT_DIR/${PIPELINE_TYPE}+baseline_20f.txt 2> $OUT_DIR/${PIPELINE_TYPE}+baseline_20f.log &
-    python main.py --train_paths=$PTB_DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE                                   > $OUT_DIR/ptb_${PIPELINE_TYPE}_20f.txt 2> $OUT_DIR/ptb_${PIPELINE_TYPE}_20f.log &
-    python main.py --train_paths=$PTB_DATA_DIR $SHARED_FLAGS --pipeline_type=$PIPELINE_TYPE --reader_gold_parses              > $OUT_DIR/ptb_${PIPELINE_TYPE}_20f_gold.txt 2> $OUT_DIR/ptb_${PIPELINE_TYPE}_20f_gold.log &
-    wait
+# Columns:
+# Run_type data_dir extra_flags
+read -r -d '' PER_RUN_VARS << EOM
+all3          $DATA_DIR
+mostfreq      $DATA_DIR     --filter_classifiers=mostfreq
+global        $DATA_DIR     --filter_classifiers=global
+perconn       $DATA_DIR     --filter_classifiers=perconn
+no_perconn    $DATA_DIR     --filter_classifiers=global,mostfreq
+no_mostfreq   $DATA_DIR     --filter_classifiers=global,perconn
+no_global     $DATA_DIR     --filter_classifiers=mostfreq,perconn
+ptb_all3      $PTB_DATA_DIR
+ptb_all3_gold $PTB_DATA_DIR --reader_gold_parses
+EOM
+
+tsp -n -L baseline bash -c "python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=baseline > $OUT_DIR/baseline.txt 2> $OUT_DIR/baseline.log"
+
+for PIPELINE in regex tregex; do
+    printf '%s\n' "$PER_RUN_VARS" | while IFS="\n" read line; do
+        read NAME DIR FLAGS <<<$line
+        echo -e "Pipeline:" $PIPELINE "\tRun type:" $NAME
+        tsp -n -L $NAME bash -c "python main.py --train_paths=$DIR $SHARED_FLAGS --pipeline_type=$PIPELINE --models_dir=../models/${PIPELINE}_${NAME} $FLAGS > $OUT_DIR/${PIPELINE}_${NAME}.txt 2>$OUT_DIR/${PIPELINE}_${NAME}.log"
+    done
+    tsp -n -L "${PIPELINE}+baseline" bash -c "python main.py --train_paths=$DATA_DIR $SHARED_FLAGS  --pipeline_type=${PIPELINE}+baseline --models_dir=../models/${PIPELINE}+baseline $FLAGS > $OUT_DIR/${PIPELINE}+baseline.txt 2>$OUT_DIR/${PIPELINE}+baseline.log"
 done
