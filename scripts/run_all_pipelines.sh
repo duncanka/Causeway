f1477ed08bca4f82bab84491ff1b3f44d59e3305
Jesse Dunietz <yavyash@gmail.com>
2016-08-29 00:10:40 -0400

    Added log dir

--- scripts/run_all_pipelines.sh
+++ scripts/run_all_pipelines.sh
@@ -2,11 +2,12 @@
 
 SEED=2961393773
 OUT_DIR=../outputs/final
+LOG_DIR=$OUT_DIR/logs
 DATA_DIR=/var/www/brat/data/finished
 PTB_DATA_DIR=/var/www/brat/data/Jeremy/PTB
 SHARED_FLAGS="--eval_with_cv --seed=$SEED --cv_folds=20"
 
-export TS_SLOTS=4
+tsp -S 4
 
 # Columns:
 # Run_type data_dir extra_flags
@@ -22,15 +23,15 @@ ptb_all3      $PTB_DATA_DIR
 ptb_all3_gold $PTB_DATA_DIR --reader_gold_parses
 EOM
 
-tsp -n -L baseline bash -c "python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=baseline > $OUT_DIR/baseline.txt 2> $OUT_DIR/baseline.log"
+tsp -n -L baseline bash -c "python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=baseline > $OUT_DIR/baseline.txt 2> $LOG_DIR/baseline.log"
 
 for PIPELINE in regex tregex; do
     printf '%s\n' "$PER_RUN_VARS" | while IFS="\n" read line; do
         read NAME DIR FLAGS <<<$line
         echo -e "Pipeline:" $PIPELINE "\tRun type:" $NAME
-        tsp -n -L $NAME bash -c "python main.py --train_paths=$DIR $SHARED_FLAGS --pipeline_type=$PIPELINE --models_dir=../models/${PIPELINE}_${NAME} $FLAGS > $OUT_DIR/${PIPELINE}_${NAME}.txt 2>$OUT_DIR/${PIPELINE}_${NAME}.log"
+        tsp -n -L $NAME bash -c "python main.py --train_paths=$DIR $SHARED_FLAGS --pipeline_type=$PIPELINE --models_dir=../models/${PIPELINE}_${NAME} $FLAGS > $OUT_DIR/${PIPELINE}_${NAME}.txt 2> $LOG_DIR/${PIPELINE}_${NAME}.log"
     done
-    tsp -n -L "${PIPELINE}+baseline" bash -c "python main.py --train_paths=$DATA_DIR $SHARED_FLAGS  --pipeline_type=${PIPELINE}+baseline --models_dir=../models/${PIPELINE}+baseline $FLAGS > $OUT_DIR/${PIPELINE}+baseline.txt 2>$OUT_DIR/${PIPELINE}+baseline.log"
+    tsp -n -L "${PIPELINE}+baseline" bash -c "python main.py --train_paths=$DATA_DIR $SHARED_FLAGS --pipeline_type=${PIPELINE}+baseline --models_dir=../models/${PIPELINE}+baseline $FLAGS > $OUT_DIR/${PIPELINE}+baseline.txt 2> $LOG_DIR/${PIPELINE}+baseline.log"
 done
 
 tsp -l
