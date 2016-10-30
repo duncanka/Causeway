f37eea0f73cde099ccaebd663569fb16ca1fd342
Jesse Dunietz <yavyash@gmail.com>
2016-10-30 11:03:47 -0400

    Split read_all to allow getting reader programmatically

--- scripts/read_all.py
+++ scripts/read_all.py
@@ -2,12 +2,16 @@ from itertools import chain
 
 from data.io import CausalityStandoffReader, DirectoryReader
 
+def get_reader():
+    return DirectoryReader((CausalityStandoffReader.FILE_PATTERN,),
+                           CausalityStandoffReader())
+
 def read_all(datadir='/var/www/brat/data/finished'):
-    reader = DirectoryReader((CausalityStandoffReader.FILE_PATTERN,),
-                             CausalityStandoffReader())
+    reader = get_reader()
     reader.open(datadir)
-    all_docs = reader.get_all()
-    all_sentences = chain.from_iterable(d.sentences for d in all_docs)
+    all_sentences = chain.from_iterable(d.sentences for d in reader)
     all_instances = chain.from_iterable(s.causation_instances
                                         for s in all_sentences)
-    return list(all_instances)
+    all_instances = list(all_instances)
+    reader.close()
+    return all_instances
