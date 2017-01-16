d3fd011bde8c2404f944f32940e90b7ddf69d4d0
Jesse Dunietz <yavyash@gmail.com>
2017-01-16 00:25:11 -0500

    Added transition oracle script

---
+++ scripts/transition_oracle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from gflags import FLAGS
+from os.path import splitext
+import sys
+
+from causeway.because_data import (CausalityStandoffReader,
+                                   CausalityOracleTransitionWriter)
+from nlpypline.data.io import DirectoryReader
+
+def main(argv):
+    FLAGS(argv) # To avoid complaints
+    files_dir = argv[1]
+    
+    reader = DirectoryReader((CausalityStandoffReader.FILE_PATTERN,),
+                             CausalityStandoffReader())
+    reader.open(files_dir)
+    documents = reader.get_all()
+    
+    writer = CausalityOracleTransitionWriter()
+    for doc in documents:
+        writer.open(splitext(doc.filename)[0] + '.trans')
+        writer.write_all_instances(doc)
+
+if __name__ == '__main__':
+    main(sys.argv)
