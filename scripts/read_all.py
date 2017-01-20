3ad907d450de72952f7d5ca2ab98afc728ee7107
Jesse Dunietz <yavyash@gmail.com>
2017-01-19 22:51:34 -0500

    Added overlapping rels reading to read_all script

--- scripts/read_all.py
+++ scripts/read_all.py
@@ -8,12 +8,20 @@ def get_reader():
     return DirectoryReader((CausalityStandoffReader.FILE_PATTERN,),
                            CausalityStandoffReader())
 
-def read_all(datadir='/var/www/brat/data/finished'):
+def read_all(datadir='/var/www/brat/data/finished', instances=True,
+             overlapping=False):
     reader = get_reader()
     reader.open(datadir)
-    all_sentences = chain.from_iterable(d.sentences for d in reader)
-    all_instances = chain.from_iterable(s.causation_instances
-                                        for s in all_sentences)
-    all_instances = list(all_instances)
-    reader.close()
-    return all_instances
+    if instances:
+        all_sentences = chain.from_iterable(d.sentences for d in reader)
+        if overlapping:
+            all_instances = chain.from_iterable(s.overlapping_rel_instances
+                                                for s in all_sentences)
+        else:
+            all_instances = chain.from_iterable(s.causation_instances
+                                                for s in all_sentences)
+        all_instances = list(all_instances)
+        reader.close()
+        return all_instances
+    else:
+        return reader.get_all()
