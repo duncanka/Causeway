7bec478a5376d16fe2b62169566592548ac144a5
Jesse Dunietz <yavyash@gmail.com>
2017-12-02 11:16:18 -0500

    Split out docs-to-instances conversion in scripts

--- scripts/read_all.py
+++ scripts/read_all.py
@@ -8,20 +8,27 @@ def get_reader(recursive=False):
     return DirectoryReader((CausalityStandoffReader.FILE_PATTERN,),
                            CausalityStandoffReader(), recursive)
 
+
+def docs_to_instances(docs, overlapping=False):
+    all_sentences = chain.from_iterable(d.sentences for d in docs)
+    if overlapping:
+        all_instances = chain.from_iterable(s.overlapping_rel_instances
+                                            for s in all_sentences)
+    else:
+        all_instances = chain.from_iterable(s.causation_instances
+                                            for s in all_sentences)
+    return list(all_instances)
+
+
 def read_all(datadir='/var/www/brat/data/finished', instances=True,
              overlapping=False, recursive=False):
     reader = get_reader(recursive)
     reader.open(datadir)
+
     if instances:
-        all_sentences = chain.from_iterable(d.sentences for d in reader)
-        if overlapping:
-            all_instances = chain.from_iterable(s.overlapping_rel_instances
-                                                for s in all_sentences)
-        else:
-            all_instances = chain.from_iterable(s.causation_instances
-                                                for s in all_sentences)
-        all_instances = list(all_instances)
-        reader.close()
-        return all_instances
+        all_instances = docs_to_instances(reader, overlapping)
     else:
-        return reader.get_all()
+        all_instances = reader.get_all()
+
+    reader.close()
+    return all_instances
