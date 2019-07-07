1fbd9ef9770c38bc69dce037da39ac5a3db33d03
Jesse Dunietz <yavyash@gmail.com>
2019-07-07 01:01:59 -0400

    Fixed issue with Stanford PTB-to-dependency conversion

--- scripts/convert-mrg.sh
+++ scripts/convert-mrg.sh
@@ -15,9 +15,10 @@ MRG_DIR=$2
 for f in $TXT_DIR/*.txt; do
     base_name=${f%.txt}
     out="$base_name.parse.gold"
-    echo "$f to $out"
+    echo "Outputting dependency tree for $f to $out"
     base_name=${base_name##*/}
+    digits=$(echo $base_name | cut -d'_' -f2)
     java -mx800m -cp "$PARSERDIR/classes:$PARSERDIR/*:" edu.stanford.nlp.trees.TreePrint \
          -format 'words,wordsAndTags,typedDependencies,penn' -options 'nonCollapsedDependencies,stem' \
-         -norm "edu.stanford.nlp.trees.BobChrisTreeNormalizer" $MRG_DIR/${base_name:0:2}/$base_name.mrg > $out
+         -norm "edu.stanford.nlp.trees.BobChrisTreeNormalizer" $MRG_DIR/${digits:0:2}/$base_name.mrg > $out
 done
